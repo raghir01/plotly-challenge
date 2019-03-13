bf917abe5490735c852dc29378d4ebcb44e2427e
raghir01 <raghigipsy@gmail.com>
2019-03-12 22:45:50 -0400

    flask in

---
+++ testflask/app.py
@@ -0,0 +1,95 @@
+import pandas as pd
+from flask import Flask, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.automap import automap_base
+
+application = Flask(__name__)
+
+#################################################
+# Database Setup
+#################################################
+
+application.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/bellybutton.sqlite"
+db = SQLAlchemy(application)
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(db.engine, reflect=True)
+
+# Save references to each table
+Samples_Metadata = Base.classes.sample_metadata
+Samples = Base.classes.samples
+
+
+@application.route("/")
+def index():
+    """Return the homepage."""
+    return render_template("index.html")
+
+
+@application.route("/names")
+def names():
+    """Return a list of sample names."""
+
+    # Use Pandas to perform the sql query
+    stmt = db.session.query(Samples).statement
+    df = pd.read_sql_query(stmt, db.session.bind)
+
+    # Return a list of the column names (sample names)
+    return jsonify(list(df.columns)[2:])
+
+
+@application.route("/metadata/<sample>")
+def sample_metadata(sample):
+    """Return the MetaData for a given sample."""
+    sel = [
+        Samples_Metadata.sample,
+        Samples_Metadata.ETHNICITY,
+        Samples_Metadata.GENDER,
+        Samples_Metadata.AGE,
+        Samples_Metadata.LOCATION,
+        Samples_Metadata.BBTYPE,
+        Samples_Metadata.WFREQ,
+    ]
+
+    results = db.session.query(*sel).filter(Samples_Metadata.sample == sample).all()
+
+    # Create a dictionary entry for each row of metadata information
+    sample_metadata = {}
+    for result in results:
+        sample_metadata["sample"] = result[0]
+        sample_metadata["ETHNICITY"] = result[1]
+        sample_metadata["GENDER"] = result[2]
+        sample_metadata["AGE"] = result[3]
+        sample_metadata["LOCATION"] = result[4]
+        sample_metadata["BBTYPE"] = result[5]
+        sample_metadata["WFREQ"] = result[6]
+
+    print(sample_metadata)
+    return jsonify(sample_metadata)
+
+
+@application.route("/samples/<sample>")
+def samples(sample):
+    """Return `otu_ids`, `otu_labels`,and `sample_values`."""
+    stmt = db.session.query(Samples).statement
+    df = pd.read_sql_query(stmt, db.session.bind)
+
+    # Filter the data based on the sample number and
+    # only keep rows with values above 1
+    sample_data = df.loc[df[sample] > 1, ["otu_id", "otu_label", sample]]
+    # Format the data to send as json
+    # otu_ids = sorted(sample_data.otu_id.values.tolist(), reverse=True)
+    # sample_values = sorted(sample_data[sample].values.tolist(), reverse=True)
+    # otu_labels = sorted(sample_data.otu_label.tolist(), reverse=True)
+    data = {
+        "otu_ids": sample_data.otu_id.values.tolist(),
+        "sample_values": sample_data[sample].values.tolist(),
+        "otu_labels": sample_data.otu_label.tolist(),
+    }
+    return jsonify(data)
+
+
+if __name__ == "__main__":
+    application.run(debug=True)
---
+++ testflask/static/js/app.js
@@ -0,0 +1,146 @@
+function buildMetadata(sample) {
+
+    d3.json("/metadata/" + sample).then(function(data){
+        var metadata = d3.select("#sample-metadata");
+        metadata.html("")
+        metadata.append("p").text("AGE: " + data["AGE"])
+        metadata.append("p").text("BBTYPE: " + data["BBTYPE"])
+        metadata.append("p").text("ETHNICITY: " + data["ETHNICITY"])
+        metadata.append("p").text("GENDER: " + data["GENDER"])
+        metadata.append("p").text("LOCATION: " + data["LOCATION"])
+        metadata.append("p").text("SAMPLEID: " + data["sample"])
+        metadata.append("p").text("WFREQ: " + data["WFREQ"])
+        drawGuage(data["WFREQ"])
+    });
+
+}
+
+function buildCharts(sample) {
+
+    d3.json("/samples/" + sample).then(function(data){
+        var otuIds = data.otu_ids.slice(0,10);
+        var otuLabel = data.otu_labels.slice(0,10);
+        var sampleValues = data.sample_values.slice(0,10);
+        var trace1 = [{
+                        "labels": otuIds,
+                        "values": sampleValues,
+                        "hovertext": otuLabel,
+                        "type": "pie"
+
+        }];
+        Plotly.newPlot("pie", trace1);
+//**************************************************************************************************************
+
+    var trace2 = {
+        mode: 'markers',
+        x: data.otu_ids,
+        y: data.sample_values,
+        text: data.otu_labels,
+        marker: {color: data.otu_ids,size: data.sample_values}
+    };
+
+     var bubble = [trace2];
+
+     var layout = {
+          height: 600,
+          width: 1200,
+          showlegend: false
+      };
+
+     Plotly.newPlot('bubble', bubble, layout);
+    });
+}
+
+
+
+function init() {
+  // Grab a reference to the dropdown select element
+  var selector = d3.select("#selDataset");
+
+  // Use the list of sample names to populate the select options
+  d3.json("/names").then((sampleNames) => {
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option")
+        .text("BB_" + sample)
+        .property("value", sample);
+    });
+
+    // Use the first sample from the list to build the initial plots
+    const firstSample = sampleNames[0];
+    buildCharts(firstSample);
+    buildMetadata(firstSample);
+  });
+}
+
+function drawGuage(level){
+    var level = level * 180 / 12;
+
+    // Trig to calc meter point
+    var degrees = 180 - level,
+         radius = .5;
+    var radians = degrees * Math.PI / 180;
+    var x = radius * Math.cos(radians);
+    var y = radius * Math.sin(radians);
+
+    // Path: may have to change to create a better triangle
+    var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+         pathX = String(x),
+         space = ' ',
+         pathY = String(y),
+         pathEnd = ' Z';
+    var path = mainPath.concat(pathX,space,pathY,pathEnd);
+
+    var data = [{ type: 'scatter',
+       x: [0], y:[0],
+        marker: {size: 28, color:'850000'},
+        showlegend: false,
+        name: 'speed',
+        text: level,
+        hoverinfo: 'text'},
+      { values: [50/6, 50/6, 50/6, 50/6, 50/6, 50/6, 50],
+      rotation: 90,
+      text: ['10-12', '8-10', '6-8', '4-6',
+                '2-4', '0-2'],
+      textinfo: 'text',
+      textposition:'inside',
+      marker: {colors:['rgba(14, 127, 0, .5)', 'rgba(110, 154, 22, .5)',
+                             'rgba(170, 202, 42, .5)', 'rgba(202, 209, 95, .5)',
+                             'rgba(210, 206, 145, .5)', 'rgba(232, 226, 202, .5)',
+                             'rgba(255, 255, 255, 0)','rgba(255, 255, 255, 0)','rgba(255, 255, 255, 0)']},
+      labels: ['10-12', '8-10', '6-8', '4-6',
+                '2-4', '0-2'],
+      hoverinfo: 'label',
+      hole: .5,
+      type: 'pie',
+      showlegend: false
+    }];
+
+    var layout = {
+      shapes:[{
+          type: 'path',
+          path: path,
+          fillcolor: '850000',
+          line: {
+            color: '850000'
+          }
+        }],
+      height: 500,
+      width: 500,
+      xaxis: {zeroline:false, showticklabels:false,
+                 showgrid: false, range: [-1, 1]},
+      yaxis: {zeroline:false, showticklabels:false,
+                 showgrid: false, range: [-1, 1]}
+    };
+
+    Plotly.newPlot('gauge', data, layout, {showSendToCloud:true});
+}
+
+
+function optionChanged(newSample) {
+  buildCharts(newSample);
+  buildMetadata(newSample);
+}
+
+// Initialize the dashboard
+init();
